dfe9ef3c5ee115be606c0147a86dc7597fc9501d
DavidAlvaradoEspe <30477423+DavidAlvaradoEspe@users.noreply.github.com>
2018-01-15 16:50:30 -0500

    TestVisualizar
    TestVisualizar Agregado

---
+++ TestVisualizar/testVisualizar.php
@@ -0,0 +1,57 @@
+<?php
+
+	$stock=$_REQUEST['txtStock'];
+	$cnn=mysqli_connect("localhost","root","");	
+	$bdd=mysqli_select_db($cnn,"bdproductos");
+	$stockDB="";
+
+	 function consultar($codigo,$cnn){
+	 	$flag=0;
+    
+        $sql="select * from producto where codigo LIKE '" .$codigo. "%'";
+        $rs=mysqli_query($cnn,$sql);
+
+        $filas=mysqli_num_rows($rs);    
+
+        if($filas==0)       
+        {
+            return ; 
+        }
+        else                                    
+        {
+            $bandera=1;                         
+        } 
+        if($bandera==1)
+        {
+        for($i=0; $i<$filas ; $i++)
+        {
+                $cursor=mysqli_fetch_array($rs);
+               $stockDB= stripslashes($cursor["stock"]) ;  
+
+          flag=1;
+            
+        }
+    } 
+    return $flag;   
+    }
+
+function testVisualizar($codigo,$cnn,$stock,$stockDB) {
+	$resultado="";
+  if(consultar($codigo,$cnn)){
+    
+    if($stock==$stockBD){
+
+   resultado="correcto";
+    }else{
+
+    	resultado="incorrecto";
+    }
+
+  }
+  echo resultado;
+
+}
+$stockDQ="3";
+$codigoDQ="1";
+testVisualizar($codigoDQ,$cnn,$stockDQ,$stockDB);
+?>
