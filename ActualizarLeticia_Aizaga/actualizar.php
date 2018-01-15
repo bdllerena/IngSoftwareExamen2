2633545cdaf9e97345d22f572234cf0f4869310a
Steven Aizaga <sxaizaga@espe.edu.ec>
2018-01-15 17:02:37 -0500

    actualizar

--- ActualizarLeticia_Aizaga/actualizar.php
+++ ActualizarLeticia_Aizaga/actualizar.php
@@ -1,22 +1,20 @@
-
 <?php
 	$interaccionbdd="";
-
 	$cnne=mysql_connect("localhost","root","");	// conectarse al motor de la base de datos
-	$bdd=mysql_select_db("espe",$cnne);		// abrir la base de datos
+	$bdd=mysql_select_db("producto",$cnne);		// abrir la base de datos
+	$id=
 
-	// ---------------------CRUD A LA BASE DE DATOS -----------------------------------------------
 
-	if(isset($_REQUEST['btnGuardar']))
+	function actualizar($, $arg_2, /* ..., */ $arg_n)
 	{
-		$sql="insert into usuario (nombre, clave, estado, codigoMD5) values('" .$nombre  . "','" .$clave ."','".$estado ."','".$codigoMD5 ."')";
-		$interaccionbdd="guardar";
+    echo "Función de ejemplo.\n";
+    return $valor_devuelto;
 	}
 
 	if(isset($_REQUEST['btnActualizar']))
 	{
 		$sql="UPDATE product SET stock='" .$nombre . "', clave='" .$clave . "', estado='" .$estado . "' where codigo="
-		.  $_REQUEST['txtCodigo']  ;
+		.  $_REQUEST['txtCodigo'] ;
 
 		$interaccionbdd=" actualizar / modificar ";
 		echo "<br> sentencia sql de " . $interaccionbdd . " ---> " . $sql ;
