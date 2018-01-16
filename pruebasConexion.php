ed4494120f8e187a1c096c7741492aecd4159752
ssllumiquinga <selena_silvi@hotmail.com>
2018-01-16 08:44:00 -0500

    test conexion BD
    añadidas mas pruebas unitarias

--- pruebasConexion.php
+++ pruebasConexion.php
@@ -4,38 +4,73 @@ include 'include/Dbconfig.php';
 
 
 class pruebasUnitarias extends UnitTestCase{
-	function testPruebasUnitarias1()
+	function TestConexion1()
 	{
 		$con = new DbConfig();
 		$c = mysqli_connect("localhost","root","","producto");
 		$this->assertEqual($c, $con->__construct());
 	}
 
-	function testPruebasUnitarias2()
+	function TestConexion2()
 	{
 		$con = new DbConfig();
 		$c = mysqli_connect("localhost","selena","","producto");
 		$this->assertEqual($c, $con->__construct());
 	}
 
-	function testPruebasUnitarias3()
+	function TestConexion3()
 	{
 		$con = new DbConfig();
 		$c = mysqli_connect("localhost","root","123","producto");
 		$this->assertEqual($c, $con->__construct());
 	}
 
-	function testPruebasUnitarias4()
+	function TestConexion4()
 	{
 		$con = new DbConfig();
 		$c = mysqli_connect("localhost","root","","usuarios");
 		$this->assertEqual($c, $con->__construct());
 	}
 
-	function testPruebasUnitarias5()
+	function TestConexions5()
 	{
 		$con = new DbConfig();
 		$c = mysqli_connect("localhost","selena","123","usuarios");
+		$this->assertEqual($c, $con->__construct());
+	}
+
+	function TestConexion6()
+	{
+		$con = new DbConfig();
+		$c = mysqli_connect("localhost","selena","123","");
+		$this->assertEqual($c, $con->__construct());
+	}
+
+	function TestConexion7()
+	{
+		$con = new DbConfig();
+		$c = mysqli_connect("localhost","selena","","usuarios");
+		$this->assertEqual($c, $con->__construct());
+	}
+
+	function TestConexion8()
+	{
+		$con = new DbConfig();
+		$c = mysqli_connect("localhost","","123","usuarios");
+		$this->assertEqual($c, $con->__construct());
+	}
+
+	function TestConexion9()
+	{
+		$con = new DbConfig();
+		$c = mysqli_connect("","selena","123","usuarios");
+		$this->assertEqual($c, $con->__construct());
+	}
+
+	function TestConexion10()
+	{
+		$con = new DbConfig();
+		$c = mysqli_connect("localhost","","","producto");
 		$this->assertEqual($c, $con->__construct());
 	}
 }
