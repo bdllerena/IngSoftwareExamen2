5caa254ea0b0a99182a9b597eb7981ca89ad3f34
DavidLlerena <davidshaek@gmail.com>
2018-01-16 08:17:23 -0500

    Cleaner code

--- index.php
+++ index.php
@@ -12,48 +12,42 @@ $result = $funciones->getData($query);
  
 <html>
 <head>    
-    <title>Modalidad</title>
+    <title>Producto</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-   <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="style.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-          <!-- Bootstrap -->
-        <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+  <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
 </head>
  
 <body>
 
    <div id="particles-js">
         <div class="btext">
-<!--<a href="add.html">Añadir Modalidad</a><br/><br/>-->
- 
     <table width='80%' border=0 class="table table-dark">
-    <tr bgcolor='#CCCCCC'>
+    <tr bgcolor='#000000'>
 		<td>id</td>
-        <td>name</td>
+        	<td>name</td>
 		<td>price</td>
 		<td>stock</td>
 		<td>acciones</td>
     </tr>
     <?php 
-    foreach ($result as $key => $res) {
-    //while($res = mysqli_fetch_array($result)) {         
+    foreach ($result as $key => $res) {       
         echo "<tr>";
-		echo "<td>".$res['id']."</td>";	
+	echo "<td>".$res['id']."</td>";	
         echo "<td>".$res['name']."</td>";	
-		echo "<td>".$res['price']."</td>";	
+	echo "<td>".$res['price']."</td>";	
         echo "<td>".$res['stock']."</td>";	
         echo "<td><a href=\"add.html\"><button class=\"btn btn-info\" autofocus>Añadir Producto</button></a>|<a href=\"edit.php?id=$res[id]\"><button class=\"btn btn-warning\">Modificar</button></a> | <a href=\"delete.php?id=$res[id]\" onClick=\"return confirm('Esta seguro que desea borrar?')\"><button class=\"btn btn-danger\">Borrar</button></a>| </td>";        
     }
     ?>
     </table>
 </div>
-
 </div>
     <script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
-
     <script>
         particlesJS.load('particles-js', 'particles.json',
         function(){
