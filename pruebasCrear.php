2542313c7453361a1e8ebd61aa5cc492470b4944
Lizeth <30345693+LizethCarolinaGuevaraJimenez@users.noreply.github.com>
2018-01-16 08:56:01 -0500

    correccion

--- pruebasCrear.php
+++ pruebasCrear.php
@@ -6,43 +6,43 @@ include 'include/Dbconfig.php';
 class pruebasUnitarias extends UnitTestCase{
 	function testPruebasUnitariasPrecio()
 	{
+		$buscar = new buscar();
 		$id=1234;
-		$con = "12.90";
-		$sql="select precio from product where codigo LIKE '" .$id. "%'";
-		$rs=mysqli_query($cnn,$sql);
-		$this->assertEqual($c, $con->__construct());
+		$con = 12.90;
+		$sql= $buscar->getData("select precio from product where codigo LIKE '" .$id. "%'");
+		$this->assertEqual($con, $sql);
 	}
-	function testPruebasUnitariasnombre()
+	function testPruebasUnitariasNombre()
 	{
+		$buscar = new buscar();
 		$id=1234;
 		$con = "teclado";
-		$sql="select nombre from product where codigo LIKE '" .$id. "%'";
-		$rs=mysqli_query($cnn,$sql);
-		$this->assertEqual($c, $con->__construct());
+		$sql= $buscar->getData("select nombre from product where codigo LIKE '" .$id. "%'");
+		$this->assertEqual($con, $sql);
 	}
 	function testPruebasUnitariasStock()
 	{
+		$buscar = new buscar();
 		$id=1234;
-		$con = 9;
-		$sql="select stock from product where codigo LIKE '" .$id. "%'";
-		$rs=mysqli_query($cnn,$sql);
-		$this->assertEqual($c, $con->__construct());
+		$con = 12
+		$sql= $buscar->getData("select stock from product where codigo LIKE '" .$id. "%'");
+		$this->assertEqual($con, $sql);
 	}
 	function testPruebasUnitariasLetras()
 	{
+		$buscar = new buscar();
 		$id=1234;
-		$con = "12sda";
-		$sql="select nombre from product where codigo LIKE '" .$id. "%'";
-		$rs=mysqli_query($cnn,$sql);
-		$this->assertEqual($c, $con->__construct());
+		$con = "lkij56";
+		$sql= $buscar->getData("select nombre from product where codigo LIKE '" .$id. "%'");
+		$this->assertEqual($con, $sql);
 	}
 	function testPruebasUnitariasNumeros()
 	{
+		$buscar = new buscar();
 		$id=1234;
-		$con = "12sda";
-		$sql="select nombre from product where codigo LIKE '" .$id. "%'";
-		$rs=mysqli_query($cnn,$sql);
-		$this->assertEqual($c, $con->__construct());
+		$con = 345F;
+		$sql= $buscar->getData("select stock from product where codigo LIKE '" .$id. "%'");
+		$this->assertEqual($con, $sql);
 	}
 
 }
