b6a09b000f92b3a605f469f2d06585534591acb1
Kelly <kelogsis_23@hotmail.com>
2018-01-16 10:40:18 -0500

    Pruebas Unitarias Eliminar
    Actualización de pruebas unitarias de eiminar.

--- Pruebas/pruebaDelete.php
+++ Pruebas/pruebaDelete.php
@@ -1,87 +1,49 @@
+<?php
+include '../simpletest/autorun.php';
+include '../include/Dbconfig.php';
 
-  <?php
-    $usuario='root';
-     $contraseña="";
+//Kelly Landázuri
 
-    $id=$_POST['id'];
-    //$name=$_POST['name'];
-    //$price=$_POST['price'];
-    //$stock=$_POST['stock'];
-    $enviar=$_POST['enviar'];
-
-    
-    try {
-      $conexion = new PDO('mysql:host=localhost;dbname=producto', $usuario, $contraseña);
-      if($opcion=='borrar'){
-//pruebas unitarias delete elemento uno
-          $idActual=1;
-          $bandera=0;
-          $result=$conexion->query("select * from product where id='$idActual';");
-          foreach ($result as $fila) {
-          $bandera=$fila[0];
-        }
-        if($bandera!=0)
-        {
-          $consulta="delete FROM producto where id='$idActual'";
-          $result=$conexion->query($consulta);
-          echo "Se elemino el registro solicitado";
-        }
-        else{
-          echo "No se encuentra dicho codigo";
-        }
-//pruebas unitarias delete elemento random
-          $idActual=20;
-          $bandera=0;
-          $result=$conexion->query("select * from product where id='$idActual';");
-          foreach ($result as $fila) {
-          $bandera=$fila[0];
-        }
-        if($bandera!=0)
-        {
-          $consulta="delete FROM producto where id='$idActual'";
-          $result=$conexion->query($consulta);
-          echo "Se elemino el registro solicitado";
-        }
-        else{
-          echo "No se encuentra dicho codigo";
-        }
-//pruebas unitarias delete elemento inexistente
-         $idActual=0;
-          $bandera=0;
-          $result=$conexion->query("select * from product where id='$idActual';");
-          foreach ($result as $fila) {
-          $bandera=$fila[0];
-        }
-        if($bandera!=0)
-        {
-          $consulta="delete FROM producto where id='$idActual'";
-          $result=$conexion->query($consulta);
-          echo "Se elemino el registro solicitado";
-        }
-        else{
-          echo "No se encuentra dicho codigo";
-        }
-//pruebas unitarias delete letras
-         $idActual='uno';
-          $bandera=0;
-          $result=$conexion->query("select * from product where id='$idActual';");
-          foreach ($result as $fila) {
-          $bandera=$fila[0];
-        }
-        if($bandera!=0)
-        {
-          $consulta="delete FROM producto where id='$idActual'";
-          $result=$conexion->query($consulta);
-          echo "Se elemino el registro solicitado";
-        }
-        else{
-          echo "No se encuentra dicho codigo";
-        }
-      }
-    } catch (PDOException $e) 
-    {
-      echo "El error es"+$e->getMessage();
-    }
-  ?>
-  
-</html>
+class pruebasUnitarias extends UnitTestCase{
+function testPruebasUnitariasIdMenor()
+  {
+    $funciones = new funciones();
+    $id=12;
+    $expected='true';
+    $result= $funciones->delete($id,'product');
+    $this->assertEqual($result, $expected, 'Prueba pasada')
+  }
+  function testPruebasUnitarisIdMayor()
+  {
+    $funciones = new funciones();
+    $id=1111;
+    $expected='true';
+    $result= $funciones->delete($id,'product');
+    $this->assertEqual($result, $expected, 'Prueba pasada')
+  }
+  function testPruebasUnitariasIdRandom()
+  {
+    $funciones = new funciones();
+    $id=243;
+    $expected='true';
+    $result= $funciones->delete($id,'product');
+    $this->assertEqual($result, $expected, 'Prueba pasada')
+  }
+  function testPruebasUnitariasIdCero()
+  {
+    $funciones = new funciones();
+    $id=0;
+    $expected='false';
+    $result= $funciones->delete($id,'product');
+    $this->assertEqual($result, $expected, 'Prueba pasada')
+  }
+  function testPruebasUnitariasIdNegativo()
+  {
+    $funciones = new funciones();
+    $id=-2;
+    $expected='false';
+    $result= $funciones->delete($id,'product');
+    $this->assertEqual($result, $expected, 'Prueba pasada')
+  }
+}
+?>
